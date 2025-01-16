a7f17df8259a6f55ce7e810f9aea172caca66dea
Lucas Carvalho <161785293+carvalhosouzalucas@users.noreply.github.com>
2025-01-16 16:20:41 -0300

    Add files via upload

---
+++ funcao.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public class FuncaoDemonstracao {
+        
+    }
+
+    public static void main(String[] args) {
+        
+        sum(10, 5);
+    }
+
+    static void sum(int x, int y) {
+        System.out.println(x + y); 
+        System.out.println(x - y); 
+        System.out.println(x / y); 
+        System.out.println(x * y); 
+    }
+}
